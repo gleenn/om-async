698ce4cc69de6dfdb8a949a3df7be74dc5c4fdbf
Glenn <gleenn@gmail.com>
2015-08-06 17:37:04 -0700

    make dist-matrix return vector instead of list; show examples in comments

--- src/clj/multidimensional_scaling/core.clj
+++ src/clj/multidimensional_scaling/core.clj
@@ -4,6 +4,7 @@
   (apply map (fn [& points] (apply vector points)) xs))
 
 (defn distance [p1 p2]
+  "(= 1.4142135623730951 (distance [1 1] [2 2]))"
   (Math/sqrt (reduce +
                      (for [i (range (count p1))]
                        (let [p1i (get p1 i)
@@ -12,10 +13,11 @@
                          (* delta delta))))))
 
 (defn distance-matrix
-  "Given a set of points, return a 2D matrix of distances between given points"
+  "Given a set of points, return a 2D matrix of distances between given points i.e.
+   (= [[0.0 1.4142135623730951] [1.4142135623730951 0.0]] (distance-matrix [[0 0] [1 1]]))
+   (= [[0.0 1.4142135623730951 1.4142135623730951]
+       [1.4142135623730951 0.0 1.4142135623730951]
+       [1.4142135623730951 1.4142135623730951 0.0]]
+     (distance-matrix [[0 0 1] [0 1 0] [1 0 0]]))"
   [points]
-  (map (fn [y] (apply vector (map (fn [x] (distance x y)) points))) points))
-
-;(= 1.4142135623730951 (distance [1 1] [2 2]))
-
-(distance-matrix [[0 0] [1 1]])
+  (apply vector (map (fn [y] (apply vector (map (fn [x] (distance x y)) points))) points)))
